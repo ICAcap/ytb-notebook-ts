19916c342a28cbec542a0968aa16f2d29cea71e9
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-05-21 23:39:34 -0400

    refactor: remove server-side code and configuration files; add initial client-side structure with layout and styling

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<h1 className="text-5xl font-bold text-center">
+				Welcome to YTB Notebook
+			</h1>
+		</div>
+	);
+}
