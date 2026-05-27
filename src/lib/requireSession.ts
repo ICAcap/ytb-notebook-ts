1199a7e66477dd847d4e16c8d7c0fc64c23493cc
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-05-27 11:21:58 -0400

    re-arrange files

--- src/lib/requireSession.ts
+++ src/lib/requireSession.ts
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { auth } from "../../auth";
+import { auth } from "./auth";
 import { headers } from "next/headers";
 
 export default async function requireSession() {
