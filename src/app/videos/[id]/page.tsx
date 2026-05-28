d1d82098bdbddc389856981dffa1b808dcb0cf86
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-05-28 14:01:40 -0400

    tweak videos/[id] page t add access guardrail

--- src/app/videos/[id]/page.tsx
+++ src/app/videos/[id]/page.tsx
@@ -1,6 +1,8 @@
 import Sidebar from "../../../../components/sidebar";
 import VideoPlayer from "./_components/VideoPlayer";
 import { prisma } from "../../../lib/prisma";
+import requireSession from "@/lib/requireSession";
+import { notFound } from "next/navigation";
 
 type video = {
 	vidId: string;
@@ -9,24 +11,18 @@ type video = {
 	collections: string[];
 };
 
-/**
- * Fetch video data based on the provided ID from the URL parameters
- * use prisma to query the database for the video details and its associated collections
- * in case the video is not found, return null to handle the "Video not found" scenario in the UI
- */
-async function getVideoById(id: string): Promise<video | null> {
-	// console.log("Fetching video with ID:", id);
-	const vid = await prisma.video.findUnique({
-		where: { videoId: id },
+async function getVideoById(userId: string, id: string): Promise<video | null> {
+	const video = await prisma.video.findFirst({
+		where: { userId, videoId: id },
 		include: { collections: true },
 	});
 
-	if (vid) {
+	if (video) {
 		return {
-			vidId: vid.videoId,
-			title: vid.title,
-			url: vid.url,
-			collections: vid.collections.map((c) => c.collectionName) || [],
+			vidId: video.videoId,
+			title: video.title,
+			url: video.url,
+			collections: video.collections.map((c) => c.collectionName) || [],
 		};
 	}
 
@@ -39,38 +35,38 @@ export default async function VidViewPage({
 }: {
 	params: { id: string };
 }) {
+	const session = await requireSession(); // unauthenticated users will be redirected to sign-in page, so session is guaranteed to be available here
 	const videoId = (await params).id;
-	const video = await getVideoById(videoId);
+	const usrId = session.user.id;
+	const video = await getVideoById(usrId, videoId);
+
+	if (!video) {
+		notFound();
+	}
 
 	return (
 		<div>
 			<Sidebar currentPath="/videos" />
 			<main className="ml-64 p-6">
-				{video ? (
-					<>
-						<h1 className="text-4xl text-wrap">{`${video.title || "Unknown Title"}`}</h1>
-						<div className="mt-4 justify-evenly">
-							<h2 className="text-xl text-gray-400">Collections:</h2>
-							<div className="flex flex-wrap gap-2 mt-2">
-								{video.collections && video.collections.length > 0 ? (
-									video.collections.map((collection, index) => (
-										<span
-											key={index}
-											className="bg-red-400 px-3 py-1 rounded-full text-sm"
-										>
-											{collection}
-										</span>
-									))
-								) : (
-									<p className="text-gray-500">No collections available.</p>
-								)}
-							</div>
-						</div>
-						<VideoPlayer url={video.url} />
-					</>
-				) : (
-					<h1 className="text-4xl text-center">Video not found</h1>
-				)}
+				<h1 className="text-4xl text-wrap">{`${video.title || "Unknown Title"}`}</h1>
+				<div className="mt-4 justify-evenly">
+					<h2 className="text-xl text-gray-400">Collections:</h2>
+					<div className="flex flex-wrap gap-2 mt-2">
+						{video.collections && video.collections.length > 0 ? (
+							video.collections.map((collection, index) => (
+								<span
+									key={index}
+									className="bg-red-400 px-3 py-1 rounded-full text-sm"
+								>
+									{collection}
+								</span>
+							))
+						) : (
+							<p className="text-gray-500">No collections available.</p>
+						)}
+					</div>
+				</div>
+				<VideoPlayer url={video.url} />
 			</main>
 		</div>
 	);
