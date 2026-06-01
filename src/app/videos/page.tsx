e988e0c7a5d34d58a29b220e6fc6fca475e47241
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-06-01 04:28:57 -0400

    feat: implement pagination for video cards and enhance VideoCard component

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -2,137 +2,46 @@ import Sidebar from "../../../components/sidebar";
 import requireSession from "../../../lib/requireSession";
 import VideoCard from "./_components/VideoCard";
 import { Metadata } from "next";
+import { Video } from "../../../generated/prisma";
+import { getVideoCardsWithPagination } from "../../../lib/dbTableAction/videoTableAction";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
 	title: "Videos",
 	description: "This is the page showing all the videos",
 };
 
+// a type that only takes data we need for video card from the Video model
+export type VideoCardType = Pick<
+	Video,
+	"url" | "title" | "lastPlayedTime" | "videoId" | "createdAt"
+>;
+
 export default async function VideoPage() {
-	await requireSession();
+	const session = await requireSession();
+	const usrId = session.user.id;
+
+	const videoCards = await getVideoCardsWithPagination(usrId, 1);
 
 	return (
 		<div className="flex min-h-screen">
 			<Sidebar currentPath="/videos" />
+
 			<main className="flex-1 p-6">
-				<h1 className="text-3xl text-center">Video Page</h1>
-				<VideoCard
-					videoUrl={"https://www.youtube.com/watch?v=Rr_hHavuq-w"}
-					title={"when phones were fun"}
-					timestamp={33002}
-					videoId={"dasfsadfa"}
-				/>
-				<VideoCard
-					videoUrl="https://www.youtube.com/shorts/Ed0FkH5cZlw"
-					title="Frutiger 2000s"
-					timestamp={22}
-					videoId={"dasfsadfa"}
-				/>
-
-				<ul className="list bg-base-100 rounded-box shadow-md">
-					<li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-						Most played songs this week
-					</li>
-
-					<li className="list-row">
-						<div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-						<div>
-							<img
-								className="size-10 rounded-box"
-								src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-							/>
-						</div>
-						<div className="list-col-grow">
-							<div>Dio Lupa</div>
-							<div className="text-xs uppercase font-semibold opacity-60">
-								Remaining Reason
-							</div>
-						</div>
-						<button className="btn btn-square btn-ghost">
-							<svg
-								className="size-[1.2em]"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-							>
-								<g
-									strokeLinejoin="round"
-									strokeLinecap="round"
-									strokeWidth="2"
-									fill="none"
-									stroke="currentColor"
-								>
-									<path d="M6 3L20 12 6 21 6 3z"></path>
-								</g>
-							</svg>
-						</button>
-					</li>
-
-					<li className="list-row">
-						<div className="text-4xl font-thin opacity-30 tabular-nums">02</div>
-						<div>
-							<img
-								className="size-10 rounded-box"
-								src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-							/>
-						</div>
-						<div className="list-col-grow">
-							<div>Ellie Beilish</div>
-							<div className="text-xs uppercase font-semibold opacity-60">
-								Bears of a fever
-							</div>
-						</div>
-						<button className="btn btn-square btn-ghost">
-							<svg
-								className="size-[1.2em]"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-							>
-								<g
-									strokeLinejoin="round"
-									strokeLinecap="round"
-									strokeWidth="2"
-									fill="none"
-									stroke="currentColor"
-								>
-									<path d="M6 3L20 12 6 21 6 3z"></path>
-								</g>
-							</svg>
-						</button>
-					</li>
-
-					<li className="list-row">
-						<div className="text-4xl font-thin opacity-30 tabular-nums">03</div>
-						<div>
-							<img
-								className="size-10 rounded-box"
-								src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-							/>
-						</div>
-						<div className="list-col-grow">
-							<div>Sabrino Gardener</div>
-							<div className="text-xs uppercase font-semibold opacity-60">
-								Cappuccino
-							</div>
-						</div>
-						<button className="btn btn-square btn-ghost">
-							<svg
-								className="size-[1.2em]"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-							>
-								<g
-									strokeLinejoin="round"
-									strokeLinecap="round"
-									strokeWidth="2"
-									fill="none"
-									stroke="currentColor"
-								>
-									<path d="M6 3L20 12 6 21 6 3z"></path>
-								</g>
-							</svg>
-						</button>
-					</li>
-				</ul>
+				<h1 className="text-3xl text-center mb-6">Your Videos</h1>
+				<Suspense
+					fallback={
+						<span className="loading loading-spinner loading-xl"></span>
+					}
+				>
+					<ul className="list bg-base-100 rounded-box shadow-md">
+						{videoCards.map((video) => (
+							<li key={video.videoId} className="list-row">
+								<VideoCard {...video} />
+							</li>
+						))}
+					</ul>
+				</Suspense>
 			</main>
 		</div>
 	);
