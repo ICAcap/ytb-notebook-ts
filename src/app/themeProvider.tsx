78dd8594454a32a32ff016b20be7d7039345172b
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-06-10 12:20:28 -0400

    feat: integrate next-themes for improved theme management and update sidebar theme options

---
+++ src/app/themeProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+// to solver flicker flash
+// reference https://www.youtube.com/watch?v=7zqI4qMDdg8&t=516s
+export function ThemeProviders({ children }: { children: React.ReactNode }) {
+	return (
+		<ThemeProvider
+			attribute="data-theme"
+			defaultTheme="light"
+			value={{ light: "retro", dark: "abyss" }}
+		>
+			{children}
+		</ThemeProvider>
+	);
+}
