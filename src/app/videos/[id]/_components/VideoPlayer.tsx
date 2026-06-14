c48db78854453628c17368cd43a49d902c5c5d84
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-06-14 18:30:24 -0400

    adjust video player size

--- src/app/videos/[id]/_components/VideoPlayer.tsx
+++ src/app/videos/[id]/_components/VideoPlayer.tsx
@@ -52,7 +52,7 @@ const VideoPlayer = ({
 	);
 
 	return (
-		<div className="w-full max-w-7xl mt-5">
+		<div className="w-full max-w-6xl mt-5">
 			<div className="aspect-video">
 				<ReactPlayer
 					ref={playerRef}
