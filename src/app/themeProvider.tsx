56b186c4ad27ff150e1fe3738f70b24bb2209887
ICAcap <58161542+ICAcap@users.noreply.github.com>
2026-06-23 10:20:57 -0400

    tweak

--- src/app/themeProvider.tsx
+++ src/app/themeProvider.tsx
@@ -3,13 +3,13 @@
 import { ThemeProvider } from "next-themes";
 
 // to solver flicker flash
-// reference https://www.youtube.com/watch?v=7zqI4qMDdg8&t=516s
+// reference https://www.youtube.com/watch?v=7zqI4qMDdg8
 export function ThemeProviders({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider
 			attribute="data-theme"
 			defaultTheme="light"
-			value={{ light: "nord", dark: "abyss" }}
+			value={{ light: "retro", dark: "abyss" }}
 		>
 			{children}
 		</ThemeProvider>
